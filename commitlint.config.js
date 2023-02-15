27f3e59ce088ed8f1f4579825c3200c79cf6a821
Zach Donnelly <zachary.donnelly@walmart.com>
2023-02-14 23:29:54 -0500

    config: add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            ['bug', 'build', 'chore', 'ci', 'config', 'docs', 'feat', 'fix', 'perf', 'refactor', 'sec', 'test']
+        ]
+    }
+}
