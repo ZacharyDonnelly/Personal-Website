1bca795d3095e5f2d6d2166c3fb197faf9f81021
Zach Donnelly <zachary.donnelly@walmart.com>
2023-02-16 02:19:28 -0500

    perf: optimize avatar image

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,4 +1,4 @@
-import { name } from '@/lib/info'
+import { description, name } from '@/lib/info'
 import { DropdownContext } from '@/lib/utils/context/DropdownContext'
 import Head from 'next/head'
 import { ReactElement, useState } from 'react'
@@ -14,10 +14,16 @@ const Layout = ({ children }: LayoutProps): ReactElement => {
         <DropdownContext.Provider value={isMenuOpen}>
             <Head>
                 <link rel="icon" href="/assets/icons/favicon.ico" />
-                <meta name="description" content="Zachary Donnelly Software Engineer" />
+                <meta name="description" content={description} />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta name="og:title" content={name} />
                 <meta name="twitter:title" content={name} />
+
+                <meta property="og:description" content={description} />
+                <meta property="og:type" content="website" />
+                <meta property="og:locale" content="en_US" />
+
+                <meta property="og:site_name" content={name} />
             </Head>
             <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
             {children}
--- src/lib/info.tsx
+++ src/lib/info.tsx
@@ -3,5 +3,6 @@ import logoDarkURL from '@/assets/icons/logo_gfx_black.png'
 import avatar from '@/assets/images/me.jpg'
 
 const name = 'Zach Donnelly'
+const description = 'Zachary Donnelly Software Engineer'
 
-export { name, logoURL, logoDarkURL, avatar }
+export { name, logoURL, logoDarkURL, avatar, description }
