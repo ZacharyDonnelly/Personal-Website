cea9e365a649e3d50567d35501db89ec247a1646
Zach Donnelly <zachary.donnelly@walmart.com>
2023-02-17 04:18:07 -0500

    chore: adjust script name

---
+++ tools/post-changes.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+npx concurrently \
+  --kill-others-on-fail \
+  --prefix "[{name}]" \
+  --names "lint,typecheck,build" \
+  --prefix-colors "bgRed.bold.white,bgGreen.bold.white,bgBlue.bold.white" \
+    "pnpm lint-staged" \
+    "pnpm typecheck" \
+    "pnpm build"
+
+git push
