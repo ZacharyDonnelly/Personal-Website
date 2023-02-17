4597faa881301f3fdc3dc866dc171305622ec414
Zach Donnelly <zachary.donnelly@walmart.com>
2023-02-17 04:14:14 -0500

    refactor: validate script

--- tools/validate.sh
+++ tools/validate.sh
@@ -3,7 +3,7 @@
 npx concurrently \
   --kill-others-on-fail \
   --prefix "[{name}]" \
-  --names "test,lint,typecheck,build" \
+  --names "lint,typecheck,build" \
   --prefix-colors "bgRed.bold.white,bgGreen.bold.white,bgBlue.bold.white,bgMagenta.bold.white" \
     "pnpm lint-staged" \
     "pnpm typecheck" \
