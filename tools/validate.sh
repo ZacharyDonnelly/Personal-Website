ebf34202c71ae371ad8897cc3c5a45097e9fbdec
Zach Donnelly <zachary.donnelly@walmart.com>
2023-02-17 04:15:30 -0500

    feat: add push to origin to validate script

--- tools/validate.sh
+++ tools/validate.sh
@@ -3,8 +3,9 @@
 npx concurrently \
   --kill-others-on-fail \
   --prefix "[{name}]" \
-  --names "lint,typecheck,build" \
+  --names "lint,typecheck,build,push" \
   --prefix-colors "bgRed.bold.white,bgGreen.bold.white,bgBlue.bold.white,bgMagenta.bold.white" \
     "pnpm lint-staged" \
     "pnpm typecheck" \
-    "pnpm build"
+    "pnpm build" \
+    "git push"
